ddda3fb5e244bb1986dc588a9c1efc20e1d59e14
zenilepl <45181384+zenilepl@users.noreply.github.com>
2018-11-23 23:04:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,52 @@
-kboosh.on('message',async message => {
-  if(message.author.bot || message.channel.type === '=bc') return;
-  let args = message.content.split(' ');
-  if(args[0] === `=bc`) {
-    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('- **أنت لا تملك الصلاحيات اللازمة لأستخدام هذا الأمر**');
-    if(!args[1]) return message.channel.send('- **يجب عليك كتابة الرسالة بعد الأمر**');
- 
-    let msgCount = 0;
-    let errorCount = 0;
-    let successCount = 0;
-    message.channel.send(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`).then(msg => {
-      message.guild.members.forEach(g => {
-        g.send(args.slice(1).join(' ')).then(() => {
-          successCount++;
-          msgCount++;
-          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
-        }).catch(e => {
-          errorCount++;
-          msgCount++;
-          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
+const Discord = require("discord.js");
+  const kboosh = new Discord.Client();
+    const kbooshtoken = "NTE1MjA0MTgxMDc4ODM1MjAw.Dtn8wQ.0IL4QtGzVvI0-epR4fGmkRqB7p0"
+      kboosh.on('ready', () => {
+        kboosh.user.setGame(`100 soon``,'https://www.twitch.tv/TEST-Broadcast');
+          console.log('Im Ready!');
+  
         });
-      });
-    });
-  }
+
+  kboosh.on('message', message => {
+    if (message.content.split(' ')[0] == '=bc')
+       message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+
+           member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
+                                                      message.delete();
+            
+                                                    });
+            
+                                                  });
+   kboosh.on("message", message => {
+       var prefix = "%";
+ 
+             var args = message.content.substring(prefix.length).split(" ");
+                if (message.content.startsWith(prefix + "=")) {
+                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+                          if (!args[1]) {
+                            
+                                 let embed3 = new Discord.RichEmbed()
+                                     .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
+                                       .setColor("#FF00FF")
+                                          message.channel.sendEmbed(embed3);
+                            
+                                        } else {
+
+                            
+                                           let embed4 = new Discord.RichEmbed()
+                                                            .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
+                                                                .setColor("#99999")
+                               
+                                                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            }
+                          }
 });
+kboosh.login(kbooshtoken);
+
       kboosh.on('guildMemberAdd', member => {
 const mohamed= member.guild.channels.get("514389881653428227");
 if(!mohamed) return;
